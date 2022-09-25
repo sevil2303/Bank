4e86b173acb7023d732210b96bc96a7cf964faf5
salam <sam@gmail.com>
2022-09-25 10:26:34 +0400

    i added menu

--- Bank/Source.cpp
+++ Bank/Source.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
 using namespace std;
 
-void ShowDebtors() {
+void ShowMenu() {
+	cout << "Show Debtors [1]" << endl;
+	cout << "Givem Credit [2]" << endl;
+	cout << "Show Debtor Debt [3]" << endl;
+	cout << "Get Debtor Payment[4]" << endl;
+}
 
+void main() {
+	
 }
