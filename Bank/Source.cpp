80b6020e5d4d7734ac551cfdf4f08ead157dc755
Sariyeva Sevil Perviz <sevilsariyeva00@gmail.com>
2022-09-25 10:57:06 +0400

    i added ShowDebtors function

--- Bank/Source.cpp
+++ Bank/Source.cpp
@@ -1,11 +1,19 @@
 #include <iostream>
+#include "Entities.h"
 using namespace std;
 
 void ShowMenu() {
 	cout << "Show Debtors [1]" << endl;
-	cout << "Givem Credit [2]" << endl;
+	cout << "Given Credit [2]" << endl;
 	cout << "Show Debtor Debt [3]" << endl;
 	cout << "Get Debtor Payment[4]" << endl; 
+}
+
+void ShowDebtors(Debtor* debtor) {
+	cout << "ID : " << debtor->id << endl;
+	cout << "NAME : " << debtor->name << endl;
+	cout << "SURNAME : " << debtor->surname << endl;
+	cout << "DEBT : " << debtor->debt << endl;
 }
 
 void main() {
